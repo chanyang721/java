dfdb1aa555b56e81d5b844147e26b81fc8081c2f
chanyang721 <chanyang721@gmail.com>
2024-04-09 14:58:45 +0900

    fix: constructor computer array and for loop

--- src/Lecture/Basic/Contructor/Constructor.java
+++ src/Lecture/Basic/Contructor/Constructor.java
@@ -7,9 +7,12 @@ public class Constructor {
         *   - 객체 지향에서 객체의 필드의 초기값을 생성 시 정의하기 위해 생성자를 사용
         *   -
         * */
-        Computer computer1 = new Computer("INTEL", "MSI", "삼성");
-        Computer computer2 = new Computer("AMD", "기가바이트", "SK");
-        System.out.println(computer1.cpu);
-        System.out.println(computer2.cpu);
+        Computer[] computers = {
+            new Computer("INTEL", "MSI", "삼성"),
+            new Computer("AMD", "기가바이트", "SK")
+        };
+        for (Computer c : computers) {
+            System.out.println("컴퓨터 정보 :" + c.cpu);
+        }
     }
 }
