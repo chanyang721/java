b74378a6e97a4d8c2e06c5f1569883dbde6fb8d1
chanyang721 <chanyang721@gmail.com>
2024-04-09 12:34:39 +0900

    add: null and gc test

--- src/Lecture/Basic/Class/Example/Product.java
+++ src/Lecture/Basic/Class/Example/Product.java
@@ -1,8 +1,11 @@
 package Lecture.Basic.Class.Example;
 
 public class Product {
+    // 필드
     String name;
     int price;
     int quantity;
+
+    // 메서드
 
 }
---
+++ src/Lecture/Basic/Null/Data.java
@@ -0,0 +1,5 @@
+package Lecture.Basic.Null;
+
+public class Data {
+    int value;
+}
