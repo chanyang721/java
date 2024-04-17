5c55471c437f5bac1a7971fa07bc612a9413a58e
chanyang721 <chanyang721@gmail.com>
2024-04-17 11:06:45 +0900

    add: 중급 인트로 & 기초, 입문 복습

---
+++ src/com/inflearn/lecture/c_intermediate/Intro.java
@@ -0,0 +1,20 @@
+package com.inflearn.lecture.c_intermediate;
+
+public class Intro {
+    public static void main(String[] args) {
+        /*
+        * 자바 입문 - 기본 문법 학습
+        * 자바 기초 - 객체 지향 특징 코드 구현
+        *           - 변수 타입: 기본형, 참조형 변수 생성 & 삭제 과정에서 메모리 작동 & 관리 방식
+        *           - 변수 종류: 정적 변수, 인스턴스 변수, 매계 변수
+        *           - Class
+        *               - oop 특징 구현: 상속, 다형성, 캡슐화, 추상화
+        *               - 접근 제어자
+        *               - 추상 클래스, 인터페이스
+        *           - 절차 지향 프로그래밍 vs 객체 지향 프로그래밍
+        * 자바 중급 - object, 불변 객체
+        *           - String, 래퍼 & Class & Enum
+        *           - 날짜와 시간, 중첩 & 내부 클래스, 예외 처리
+        * */
+    }
+}
