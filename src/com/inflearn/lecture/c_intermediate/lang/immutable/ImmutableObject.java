edfd50685e06edeb17f9a563a4b760028fdb55f6
chanyang721 <chanyang721@gmail.com>
2024-04-29 11:36:18 +0900

    move: Immutable package

--- src/com/inflearn/lecture/c_intermediate/lang/immutable/ImmutableObject.java
+++ src/com/inflearn/lecture/c_intermediate/lang/immutable/ImmutableObject.java
@@ -1,8 +1,8 @@
-package com.inflearn.lecture.c_intermediate.lang.object.immutable;
+package com.inflearn.lecture.c_intermediate.lang.immutable;
 
 public class ImmutableObject {
-    private final String name; // final 키워드로 상수 처리
-    private final int finalNumber;
+    private final String name; // 상수
+    private final int finalNumber; // 상수
 
     public ImmutableObject(String name, int finalNumber) {
         this.name = name; // 생성자에서 할당 후 변경 불가능
@@ -10,9 +10,7 @@ public class ImmutableObject {
     }
 
     public static void main(String[] args) {
-        /*
-        *
-        * */
+
     }
 
     public int getNumber () {
@@ -25,7 +23,7 @@ public class ImmutableObject {
 
     public ImmutableObject add(int number) {
         /*
-        * 불변 객체의 값 수정은 새로운 객체 전달을 의미
+        * 불변 객체의 값 수정은 새로운 객체 전달
         * */
         int newNumber = this.finalNumber + number; // 새로운 불변 값 생성
         return new ImmutableObject(this.name, newNumber); // 새로운 객체 리턴
